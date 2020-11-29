490ba25540bdab00d46ddb41ea2113bee12de130
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2020-11-29 10:31:38 +0000

    Adding env.py again

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("MONGO_DBNAME", "VP")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:4EDUt5qh5UR5cIW4@vacationplanner.1hd5f.mongodb.net/VacationPlanner?retryWrites=true&w=majority")
+os.environ.setdefault("SECRET_KEY", "x}*9RLy7t>z5m/XE")
