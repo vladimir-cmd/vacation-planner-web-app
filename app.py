0b804ef0bff1427852a31b5bfe3789c57109c937
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2020-12-12 18:40:19 +0000

    Updating vacation days successfully

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
+
+from bson.objectid import ObjectId
 from flask import (
     Flask, flash, render_template,
     request, session, redirect, url_for)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from pip._vendor.msgpack.fallback import xrange
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
-
+from datetime import datetime,date,timedelta
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -36,7 +41,8 @@ def register():
             return redirect(url_for("register"))
         register_user = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(request.form.get("password")),
+            "vacation_days": 25
         }
 
         mongo.db.users.insert_one(register_user)
@@ -99,6 +105,7 @@ def logout():
 def add_entry():
     if request.method == "POST":
         half_day = "on" if request.form.get("half_day") else "off"
+        user = mongo.db.users.find_one({ 'username' : session["user"]})
         entry = {
             "category_name": request.form.get("category_name"),
             "entry_type": request.form.get("entry_type"),
@@ -110,6 +117,24 @@ def add_entry():
         }
         mongo.db.entries.insert_one(entry)
         flash("Entry successfully added")
+
+        start_date = datetime.strptime(request.form.get("start_date"), '%d %b, %Y')
+        end_date = datetime.strptime(request.form.get("end_date"), '%d %b, %Y')
+
+        day_generator = (start_date + timedelta(x + 1) for x in xrange((end_date - start_date).days))
+        how_many_days = sum(1 for day in day_generator if day.weekday() < 5) + 1
+
+        print("How many days {} to deduct from original: {}".format(how_many_days, user['vacation_days']))
+
+        # how_many_days = (end_date - start_date).days
+        update_user_days = user['vacation_days'] - how_many_days
+        update_vacation_days = {
+            "username": user['username'],
+            "password": user["password"],
+            "vacation_days": update_user_days
+        }
+        mongo.db.users.update({"username": user['username']}, update_vacation_days)
+
         return redirect(url_for("manage_entries"))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
@@ -121,6 +146,53 @@ def add_entry():
 def edit_entry(entry_id):
     if request.method == "POST":
         half_day = "on" if request.form.get("half_day") else "off"
+        user = mongo.db.users.find_one({ 'username' : session["user"]})
+        print("Number of vacation days before edit: {}".format(user['vacation_days']))
+
+        # First get the current vacation date frame and update vacation days with that number
+        entry = mongo.db.entries.find_one({"_id": ObjectId(entry_id)})
+        start_date_from_entry = datetime.strptime(entry["start_date"], '%d %b, %Y')
+        end_date_from_entry = datetime.strptime(entry["end_date"], '%d %b, %Y')
+
+        print("Start Date from Entry: {}".format(start_date_from_entry))
+        print("End Date from Entry: {}".format(end_date_from_entry))
+
+        day_generator = (start_date_from_entry + timedelta(x + 1) for x in xrange((end_date_from_entry - start_date_from_entry).days))
+        how_many_days_to_add = sum(1 for day in day_generator if day.weekday() < 5) + 1
+
+        print("How many days {} to add back to original: {}".format(how_many_days_to_add, user['vacation_days']))
+
+        update_user_days_before_new_deduction = user['vacation_days'] + how_many_days_to_add
+        print("The new status of vacation days: {}".format(update_user_days_before_new_deduction))
+        update_vacation_days = {
+            "username": user['username'],
+            "password": user["password"],
+            "vacation_days": update_user_days_before_new_deduction
+        }
+
+        mongo.db.users.update({"username": user['username']}, update_vacation_days)
+
+        user = mongo.db.users.find_one({ 'username' : session["user"]})
+        print("Number of vacation days after first edit: {}".format(user['vacation_days']))
+
+        # Next we deduct new amount of vacation days
+        start_date = datetime.strptime(request.form.get("start_date"), '%d %b, %Y')
+        end_date = datetime.strptime(request.form.get("end_date"), '%d %b, %Y')
+
+        day_generator = (start_date + timedelta(x + 1) for x in xrange((end_date - start_date).days))
+        how_many_days = sum(1 for day in day_generator if day.weekday() < 5) + 1
+        print("How many days {} to add back to original: {}".format(how_many_days, user['vacation_days']))
+
+        # how_many_days = (end_date - start_date).days
+        update_user_days = user['vacation_days'] - how_many_days
+        print("The new status of vacation days after second update: {}".format(update_user_days))
+        update_vacation_days = {
+            "username": user['username'],
+            "password": user["password"],
+            "vacation_days": update_user_days
+        }
+        mongo.db.users.update({"username": user['username']}, update_vacation_days)
+
         update = {
             "category_name": request.form.get("category_name"),
             "entry_type": request.form.get("entry_type"),
@@ -130,11 +202,11 @@ def edit_entry(entry_id):
             "half_day": half_day,
             "created_by": session["user"]
         }
+
         mongo.db.entries.update({"_id": ObjectId(entry_id)}, update)
         flash("Entry successfully Updated!")
 
     entry = mongo.db.entries.find_one({"_id": ObjectId(entry_id)})
-
     categories = mongo.db.categories.find().sort("category_name", 1)
     vacation_types = mongo.db.vacation_types.find().sort("entry_type", 1)
     return render_template("edit_entry.html", categories=categories, vacation_types=vacation_types, entry=entry)
