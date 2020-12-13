d405e2a2decd39645401e8afbc52551944d54b3c
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2020-12-13 16:37:00 +0000

    Delete Entry update vacation days and confirm password when register

--- app.py
+++ app.py
@@ -39,18 +39,25 @@ def register():
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-        register_user = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password")),
-            "vacation_days": 25
-        }
+        # Check if password match
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+        if password == confirm_password:
+            register_user = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password")),
+                "vacation_days": 25
+            }
 
-        mongo.db.users.insert_one(register_user)
+            mongo.db.users.insert_one(register_user)
 
-        # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Registration successful!")
-        return redirect(url_for("profile", username=session["user"]))
+            # put the new user into 'session' cookie
+            session["user"] = request.form.get("username").lower()
+            flash("Registration successful!")
+            return redirect(url_for("profile", username=session["user"]))
+        else:
+            flash("Password must match. Try again")
+            return redirect(url_for("register"))
     return render_template("register.html")
 
 
@@ -124,8 +131,6 @@ def add_entry():
         day_generator = (start_date + timedelta(x + 1) for x in xrange((end_date - start_date).days))
         how_many_days = sum(1 for day in day_generator if day.weekday() < 5) + 1
 
-        print("How many days {} to deduct from original: {}".format(how_many_days, user['vacation_days']))
-
         # how_many_days = (end_date - start_date).days
         update_user_days = user['vacation_days'] - how_many_days
         update_vacation_days = {
@@ -147,23 +152,17 @@ def edit_entry(entry_id):
     if request.method == "POST":
         half_day = "on" if request.form.get("half_day") else "off"
         user = mongo.db.users.find_one({ 'username' : session["user"]})
-        print("Number of vacation days before edit: {}".format(user['vacation_days']))
 
         # First get the current vacation date frame and update vacation days with that number
         entry = mongo.db.entries.find_one({"_id": ObjectId(entry_id)})
         start_date_from_entry = datetime.strptime(entry["start_date"], '%d %b, %Y')
         end_date_from_entry = datetime.strptime(entry["end_date"], '%d %b, %Y')
 
-        print("Start Date from Entry: {}".format(start_date_from_entry))
-        print("End Date from Entry: {}".format(end_date_from_entry))
-
-        day_generator = (start_date_from_entry + timedelta(x + 1) for x in xrange((end_date_from_entry - start_date_from_entry).days))
+        day_generator = (start_date_from_entry + timedelta(x + 1) for x in xrange(
+            (end_date_from_entry - start_date_from_entry).days))
         how_many_days_to_add = sum(1 for day in day_generator if day.weekday() < 5) + 1
 
-        print("How many days {} to add back to original: {}".format(how_many_days_to_add, user['vacation_days']))
-
         update_user_days_before_new_deduction = user['vacation_days'] + how_many_days_to_add
-        print("The new status of vacation days: {}".format(update_user_days_before_new_deduction))
         update_vacation_days = {
             "username": user['username'],
             "password": user["password"],
@@ -171,9 +170,7 @@ def edit_entry(entry_id):
         }
 
         mongo.db.users.update({"username": user['username']}, update_vacation_days)
-
         user = mongo.db.users.find_one({ 'username' : session["user"]})
-        print("Number of vacation days after first edit: {}".format(user['vacation_days']))
 
         # Next we deduct new amount of vacation days
         start_date = datetime.strptime(request.form.get("start_date"), '%d %b, %Y')
@@ -181,11 +178,9 @@ def edit_entry(entry_id):
 
         day_generator = (start_date + timedelta(x + 1) for x in xrange((end_date - start_date).days))
         how_many_days = sum(1 for day in day_generator if day.weekday() < 5) + 1
-        print("How many days {} to add back to original: {}".format(how_many_days, user['vacation_days']))
 
         # how_many_days = (end_date - start_date).days
         update_user_days = user['vacation_days'] - how_many_days
-        print("The new status of vacation days after second update: {}".format(update_user_days))
         update_vacation_days = {
             "username": user['username'],
             "password": user["password"],
@@ -214,6 +209,26 @@ def edit_entry(entry_id):
 
 @app.route("/delete_entry/<entry_id>")
 def delete_entry(entry_id):
+
+    # First lets update the vacation days
+    user = mongo.db.users.find_one({ 'username' : session["user"]})
+    entry = mongo.db.entries.find_one({"_id": ObjectId(entry_id)})
+    start_date_from_entry = datetime.strptime(entry["start_date"], '%d %b, %Y')
+    end_date_from_entry = datetime.strptime(entry["end_date"], '%d %b, %Y')
+
+    day_generator = (start_date_from_entry + timedelta(x + 1) for x in xrange(
+        (end_date_from_entry - start_date_from_entry).days))
+    how_many_days_to_add = sum(1 for day in day_generator if day.weekday() < 5) + 1
+
+    update_user_days_before_new_deduction = user['vacation_days'] + how_many_days_to_add
+    update_vacation_days = {
+        "username": user['username'],
+        "password": user["password"],
+        "vacation_days": update_user_days_before_new_deduction
+    }
+    mongo.db.users.update({"username": user['username']}, update_vacation_days)
+
+    # Next Lets delete the user entry
     mongo.db.entries.remove({"_id": ObjectId(entry_id)})
     flash("Task Successfully Deleted!")
     return redirect(url_for("manage_entries"))
